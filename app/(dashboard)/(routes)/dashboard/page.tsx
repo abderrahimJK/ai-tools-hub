6af179ed691864d57edd94b653788136f9118f0b
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-09-14 16:32:38 +0100

    project setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage = ()=> {
+  return (
+    <h1>Dashboard</h1>
+  )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const LandingPage = () => {
+  return (
+    <h1>Landing Page</h1>
+  )
+}
+
+export default LandingPage;
