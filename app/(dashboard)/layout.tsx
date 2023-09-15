2e85d80388ac5764b43e8514a5cdef009601df81
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-09-15 11:45:56 +0100

    added sidebar & navbar styling and content

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 
 const DashboardLayout = ({
                              children
@@ -9,9 +11,7 @@ const DashboardLayout = ({
   return(
       <div className="h-full relative">
           <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-              <div className="text-white">
-                  Hello SideBar
-              </div>
+              <Sidebar/>
           </div>
           <main className="md:pl-72">
               <Navbar />
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,18 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <h1>Landing Page</h1>
+      <div>
+        <h1>Landing Page</h1>
+        <Link href="/sign-in">
+          <button>Sign-in</button>
+        </Link>
+        <Link href="/sign-up">
+          <button>Register</button>
+        </Link>
+      </div>
   )
 }
 
